acbd107e6999e46cdf306326fd28c31b9b72eacc
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2019-12-14 22:15:17 -0300

    modificacoes no BD e formulario de usuario

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, flash
-
+from flask import Flask, render_template, url_for, request, flash, redirect
+from bd import *
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    dados = {}
+    dados['url_form'] = url_for('usuario_inserir')
+    return render_template('usuario_form.html', dados=dados)
 
-@app.route('/usuario/insert', methods=['POST'])
+@app.route('/usuario/inserir', methods=['POST'])
 def usuario_inserir():
-    data = None #funcao do luiz para inserir
-    if data:
-        flash(f'usuario {data.nome}, criado com sucesso!')
+    dados = {}
+    dados['url_form'] = url_for('usuario_inserir')
+    adicionar_usuario(request)
+    return render_template('usuario_form.html', dados=dados)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
