900d7ff9ac3a984a425e5e20a61ea061aba45748
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2019-12-15 00:23:48 -0300

    verificar se login e valido implementado

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from bd import *
 app = Flask(__name__)
+app.secret_key = """Kazakhstan greatest country in the world
+All other countries are run by little girls
+Kazakhstan number one exporter of potassium"""
 
-
+usuario_logado = None
 @app.route('/')
 def index():
+    print(usuario_logado)
     dados = {}
-    dados['url_form'] = url_for('usuario_inserir')
-    return render_template('usuario_form.html', dados=dados)
+    dados['url_form'] = url_for('usuario_login')
+    return render_template('teste.html', dados=dados)
+
+@app.route('/login', methods=['POST'])
+def usuario_login():
+    usuario_logado = verificar_login(request)
+    dados = {}
+    if usuario_logado:
+        dados = {}
+        dados['url_form'] = url_for('usuario_login')
+        print("logou")
+        dados['url_form'] = url_for('usuario_inserir')
+        return render_template('usuario_form.html', dados=dados)
+    else:
+        print("nao logou")
+        return render_template('usuario_form.html', dados=dados)
+
 
 @app.route('/usuario/inserir', methods=['POST'])
 def usuario_inserir():
     dados = {}
     dados['url_form'] = url_for('usuario_inserir')
-    adicionar_usuario(request)
+    data = adicionar_usuario(request)
+    if data:
+        flash(f'Usuario: {data.nome} criado com sucesso')
     return render_template('usuario_form.html', dados=dados)
 
 if __name__ == '__main__':
